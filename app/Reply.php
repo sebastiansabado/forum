1b29bd8ec34e90cc6437f2f4f25443f4cc81e381
Sebastian Sabado <sebastiansabado2@yahoo.com>
2017-07-10 14:57:00 -0700

    Thread Owner Testing

--- app/Reply.php
+++ app/Reply.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Reply extends Model
 {
-    //
+    public function owner()
+    {
+
+    	return $this->belongsTo(User::class, 'user_id');
+
+    }
+
 }
